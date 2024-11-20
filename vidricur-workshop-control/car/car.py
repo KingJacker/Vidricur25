a7ea3ee0645c345ec9a1b8bf265b5a9af91902ce
dan.heller <dan.heller@students.fhnw.ch>
2024-11-20 12:36:10 +0000

    reworked control logic

--- vidricur-workshop-control/car/car.py
+++ vidricur-workshop-control/car/car.py
@@ -32,6 +32,11 @@ class Car(metaclass=Singleton):
         self.engine = Engine(self.esc_pwm)
         self.wheel = Wheel(self.steering_pwm)
 
+        self.max_angle = 0.5
+        self.max_speed = 0
+
+
+
     async def start(self):
         await self.wheel.set_angle(90)
         await self.engine.setSpeed(0)
@@ -42,59 +47,98 @@ class Car(metaclass=Singleton):
         await self.sio.emit("message", json.dumps(message))
 
     async def handleEvent(self, event):
-        is_action = False
-        was_successful = False
-        error_message = ""
-        
-        # If the event is a move event
-        if event["action"] == "move":
-            # Get angle of the wheels
-            angle = int(event["options"]["angle"])
+        logger.debug(event)
+
+        # CONTROl EVENT
+        if event["source"] == "control":
+            control = event['content']['control']
             
-            # Decrease the event
-            if event["options"]["speed"] == -1:
-                # Decrease the engine speed
-                await self.engine.decreaseSpeed()
-                logger.info("Action - Decrease engine speed")
-                is_action = True
-                was_successful = True
+            # STOP
+            if control['space'] != 0:
+                await self.engine.halt()
+                logger.info("Action: Stop")
+            
+            # FORWARD / BACKWARD
+            if control['w'] > 0 and control['s'] == 0:
+                await self.engine.setSpeed(control['w'] * self.max_speed)
+                logger.info("Action: Forward")
+            elif control['s'] > 0 and control['w'] == 0:
+                await self.engine.setSpeed(-1 * control['s'] * self.max_speed)
+                logger.info("Action: Backward")
+            else:
+                await self.engine.setSpeed(0)
+                # logger.info("Action: Reset Speed")
+            
+            # LEFT / RIGHT
+            if control['a'] > 0 and control['d'] == 0:
+                perc = control['a'] * self.max_angle
+                await self.wheel.set_angle_percent(perc)
+                logger.info(f"Action: Left ({perc})")
+            elif control['d'] > 0 and control['a'] == 0:
+                perc = -1 * control['d'] * self.max_angle
+                await self.wheel.set_angle_percent(perc)
+                logger.info(f"Action: Right ({perc})")
+            else:
+                await self.wheel.set_angle_percent(0)
+                # logger.info("Action: Reset Angle")
+
+        # if event["source"] == "remote":
+        #     logger.debug(event)
+
+
+        # is_action = False
+        # was_successful = False
+        # error_message = ""
+        
+        # # If the event is a move event
+        # if event["action"] == "move":
+        #     # Get angle of the wheels
+        #     angle = int(event["options"]["angle"])
+            
+        #     # Decrease the event
+        #     if event["options"]["speed"] == -1:
+        #         # Decrease the engine speed
+        #         await self.engine.decreaseSpeed()
+        #         logger.info("Action - Decrease engine speed")
+        #         is_action = True
+        #         was_successful = True
                 
-            # Increase the event
-            elif event["options"]["speed"] == 1:
-                # Increase the engine speed
-                await self.engine.increaseSpeed()
-                logger.info("Action - Increase engine speed")
-                is_action = True
-                was_successful = True
+        #     # Increase the event
+        #     elif event["options"]["speed"] == 1:
+        #         # Increase the engine speed
+        #         await self.engine.increaseSpeed()
+        #         logger.info("Action - Increase engine speed")
+        #         is_action = True
+        #         was_successful = True
             
-            # If there is a new angle compared to the current angle
-            if self.current_angle != angle:
-                # Set new angle of the wheels
-                await self.wheel.set_angle(int(event["options"]["angle"]))
-                self.current_angle = angle
-                logger.info("Action - Set wheel angle: " + str(angle))
-                is_action = True
-                was_successful = True
+        #     # If there is a new angle compared to the current angle
+        #     if self.current_angle != angle:
+        #         # Set new angle of the wheels
+        #         await self.wheel.set_angle(int(event["options"]["angle"]))
+        #         self.current_angle = angle
+        #         logger.info("Action - Set wheel angle: " + str(angle))
+        #         is_action = True
+        #         was_successful = True
                 
-        # Stop the car event
-        elif event["action"] == "stop":
-            await self.engine.halt()
-            logger.info("Action - Stop")
-            is_action = True
-            was_successful = True
+        # # Stop the car event
+        # elif event["action"] == "stop":
+        #     await self.engine.halt()
+        #     logger.info("Action - Stop")
+        #     is_action = True
+        #     was_successful = True
             
-        # If the websocket disconnect -> Emerency halt of the car
-        elif event["action"] == "disconnect":
-            await self.engine.halt()
-            logger.info("Action - Disconnect: Emergency halt!")
-            is_action = True
-            was_successful = True
+        # # If the websocket disconnect -> Emerency halt of the car
+        # elif event["action"] == "disconnect":
+        #     await self.engine.halt()
+        #     logger.info("Action - Disconnect: Emergency halt!")
+        #     is_action = True
+        #     was_successful = True
         
-        current_speed = await self.engine.getSpeed()
+        # current_speed = await self.engine.getSpeed()
         
-        # Action reponse
-        if (is_action):
-            await self.send_message({"source": "car", "ok": was_successful, "error": error_message, "current_angle": self.current_angle, "current_speed": current_speed})
-        else:
-            await self.send_message({"source": "car", "ok": False, "error": "Invalid action given", "current_angle": self.current_angle, "current_speed": current_speed})
+        # # Action reponse
+        # if (is_action):
+        #     await self.send_message({"source": "car", "ok": was_successful, "error": error_message, "current_angle": self.current_angle, "current_speed": current_speed})
+        # else:
+        #     await self.send_message({"source": "car", "ok": False, "error": "Invalid action given", "current_angle": self.current_angle, "current_speed": current_speed})
         
