33a1604ce8fbdafbf85d20faa7a933e94c6cd65d
KingJacker <dh14300.1@gamil.com>
2025-03-11 13:19:54 +0100

    config & handler

---
+++ vidricur-workshop-control/car/config_handler.py
@@ -0,0 +1,22 @@
+import json
+
+CONFIG_PATH = "config.json"
+
+def read_config():
+    with open(CONFIG_PATH, "r") as file:
+        return json.loads(file.read())
+    
+def write_config(config):
+    with open(CONFIG_PATH, "w") as file:
+        file.write(json.dumps(config))
+
+def get_steering_config():
+    config = read_config()
+    return config["steering"]["front"], config["steering"]["front"], config["steering"]["max"]
+
+def get_float_config():
+    config = read_config()
+    return config["floats"]["left"]["up"], config["floats"]["left"]["down"], config["floats"]["right"]["up"], config["floats"]["right"]["down"]
+
+def get_config():
+    return read_config()
