229d6f226bf75416328a817f5b53ae7579efbd93
KingJacker <dh14300.1@gamil.com>
2025-05-08 16:08:57 +0200

    started on endstop

--- vidricur-workshop-control/car/arm.py
+++ vidricur-workshop-control/car/arm.py
@@ -10,13 +10,29 @@ class Arm():
 		self.direction = 0
 		self.arm_motor = PWMChannel(pca, 15)
 		self.gpio_handler = gpio_handler
-		self.pin = 16
+		self.motor_pin = 16
+		# self.endstop_pin = None # TODO: add home pin
+		# self.homing = False
 
-		lgpio.gpio_claim_output(self.gpio_handler, self.pin)
+		lgpio.gpio_claim_output(self.gpio_handler, self.motor_pin)
+		# lgpio.gpio_claim_output(self.gpio_handler, self.endstop_pin)
+
+	
+	# Homing Sequence
+	# def home(self):
+	# 	logger.debug("Homing arm")
+	# 	self.homing = True
+	# 	if self.endstop_pin:
+	# 		# stop
+	# 	else:
+	# 		# set direction towards endstop
+	# 		# move 
+
+	
 
 	def set_direction(self, direction):
 		self.direction = direction
-		lgpio.gpio_write(self.gpio_handler, self.pin, self.direction)
+		lgpio.gpio_write(self.gpio_handler, self.motor_pin, self.direction)
 
 		# GPIO erstellen / setzen
 
